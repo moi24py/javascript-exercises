20e4f539594cfcd9823cd95f8c040b4b71f4c240
moi24py <moi24py@gmail.com>
2026-09-01 17:02:37 +0200

    add function to reverse a string

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let arr = str.split("");
 
+    let newStr = "";
+
+    for(let i=arr.length-1; i >= 0; i--){
+        newStr += arr[i];
+    }
+    return newStr;
 };
 
 // Do not edit below this line

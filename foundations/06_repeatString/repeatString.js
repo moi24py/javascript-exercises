216a1b259872ec2c2877ee0f55a2f2f173d888ae
moi24py <moi24py@gmail.com>
2026-09-01 16:50:17 +0200

    add function to repeat a string x times

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,6 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) return "ERROR";
+    for(let i=0; i < num; i++) console.log(string);
 };
 
 // Do not edit below this line

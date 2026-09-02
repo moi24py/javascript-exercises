d5ec021f42c07c21019d89c83a2c2b68e87740bc
moi24py <moi24py@gmail.com>
2026-09-02 11:44:38 +0200

    Add function to remove number(s) from an array

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...numbers) {
+    let newArr = [];
+    newArr = arr.filter((n) => !numbers.includes(n));
+    return newArr;
 };
 
 // Do not edit below this line

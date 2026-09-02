6b91b2302668ede84fafba0ad4fe947850e8bf36
moi24py <moi24py@gmail.com>
2026-09-02 12:07:06 +0200

    Define funtion to sum integers from m to n included

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR";
+    if ((num1 <= 0) || (num2 <= 0)) return "ERROR";
+    
+    let min = num1 < num2 ? num1 : num2;
+    console.log(min);
+    let max = num1 > num2 ? num1 : num2;
+    console.log(max);
+    let tot = 0;
+    for(let i = min; i <= max; i++){
+        tot += i;
+    }
+    return tot;
 };
 
 // Do not edit below this line

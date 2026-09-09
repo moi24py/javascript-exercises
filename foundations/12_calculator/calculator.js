e9aa550fe8e8561787de4985a7abc7ec4076451a
moi24py <moi24py@gmail.com>
2026-09-09 16:17:51 +0200

    Add functions to calculator

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b ;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce( (tot, num) => tot += num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce( (tot, num) => tot *= num, 1);
 };
 
-const power = function() {
-	
+const power = function(x, n) {
+	return Math.pow(x, n);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  let fac = 1;
+	while (n > 0) {
+    fac *= n;
+    n--;
+  }
+  return fac;
 };
 
 // Do not edit below this line

3a66c8fcb8d83ba68669493d426a091e1138e082
moi24py <moi24py@gmail.com>
2026-09-12 15:21:57 +0200

    Add function to return the oldest person

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,30 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+        const currDate = new Date().getFullYear();
+        let yearsLived1 = 0;
+        let yearsLived2 = 0;
+        arr.sort(
+            (a,b) => {
+                if(a?.yearOfDeath == undefined) {
+                    yearsLived1 = currDate - a.yearOfBirth;
+                    console.log(yearsLived1);
+                } else {
+                    yearsLived1 = a.yearOfDeath - a.yearOfBirth;
+                    console.log(yearsLived1);
+                }
+                if(b?.yearOfDeath == undefined){
+                    yearsLived2 = currDate - b.yearOfBirth;
+                    console.log(yearsLived2);
+                } else {
+                    yearsLived2 = b.yearOfDeath - b.yearOfBirth;
+                    console.log(yearsLived2);
+                }
+                
+                return yearsLived1 < yearsLived2 ? 1 : -1;
+            }
+        )
+            return arr[0];
+    };
 
-};
 
 // Do not edit below this line
 module.exports = findTheOldest;

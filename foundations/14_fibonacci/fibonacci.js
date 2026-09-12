04f185e93663b29b4540a64add02e0fc2a25f796
moi24py <moi24py@gmail.com>
2026-09-12 15:07:51 +0200

    Add function to return the i-th element of the Fibonacci series

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(elementh) {
+    num = Number(elementh);
+    
+    if (num < 0 ) return "OOPS";
+    if (num == 0) return 0;
+    if (num == 1) return 1;
 
+    let fibSeries = [1,1,];
+    if (num > 1){
+        for (let i = 1; i <= 50; i++){
+            fibSeries.push(fibSeries[i] + fibSeries[i-1]); 
+        }
+    return fibSeries.at(num-1);
+    }
 };
 
 // Do not edit below this line
